b1d43037fe3fb55b1ff24a051f13a96e6547cb2b
SUMAREDDY <lavanurusaisuma2003@gmail.com>
2023-09-07 09:44:28 +0530

    mvc

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const port=8000;
 const app=express();
+const userRouters=require('./router/users')
+app.use('/',userRouters);
+//app.use('/api',userRouters);
 
 app.listen(port,function(err){
     if(err){
         console.log("Error is running on server");
     }
-    //console.log("server is running on port: ${port}");
     console.log(`server is running on port: ${port}`);
 })
